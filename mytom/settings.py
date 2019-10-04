a311a226d1b7f7808fac391e675497f768f29316
MelissaGraham <melissalynngraham@gmail.com>
2019-10-04 15:52:54 -0700

    fixed again

--- mytom/settings.py
+++ mytom/settings.py
@@ -30,8 +30,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = MY_SECRET_KEY
-SECRET_KEY = os.getenv('MY_SECRET_KEY',local_settings.MY_SECRET_KEY)
+try: 
+    SECRET_KEY = MY_SECRET_KEY
+except:
+    SECRET_KEY = os.getenv('MY_SECRET_KEY')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
